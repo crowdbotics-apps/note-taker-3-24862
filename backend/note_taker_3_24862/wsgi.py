1824959d602be221908959e3060da35ca3c4f73a
Crowdbotics <team@crowdbotics.com>
2021-03-05 05:47:05 +0000

    Update models

--- backend/note_taker_3_24862/wsgi.py
+++ backend/note_taker_3_24862/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'note_taker_3_24862.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "note_taker_3_24862.settings")
 
 application = get_wsgi_application()
